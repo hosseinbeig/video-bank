3b06019abac73623f089c2f2c713dab6b6e7c92a
hossein <h.beig@yahoo.com>
2018-12-13 15:01:14 +0800

    video item css init

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = props => {
-  return <div>{props.videos.length}</div>;
+const VideoList = ({ videos }) => {
+  const renderdList = videos.map(video => {
+    return <VideoItem video={video} />;
+  });
+  return <div className="ui relaxed divided list">{renderdList}</div>;
 };
 
 export default VideoList;
